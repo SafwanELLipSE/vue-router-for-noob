e0cb3c4e9522bfa586621cc8507c5430f04c6c3d
rubab123552 <safwanhassan13@gmail.com>
2021-08-13 17:08:57 +0600

    first commit

---
+++ src/components/Profile.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="hello">
+        <h2>Profile Page({{user_id}})</h2>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Profile',
+    data () {
+        return {
+            user_id: this.$route.params.user_id
+        }
+    },
+    methods: {
+        updateId(){
+            this.user_id = this.$route.params.user_id
+        }
+    },
+    watch:{
+        $route: 'updateId'
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h2{
+        color: green;
+    }
+</style>
